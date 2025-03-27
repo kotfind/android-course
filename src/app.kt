11c482548883f226c39074b80ad87b104b44a6d4
kotfind <kotfind@yandex.ru>
2025-03-27 20:23:48 +0300

    Option Menu Demo

--- src/app.kt
+++ src/app.kt
@@ -7,13 +7,63 @@ import androidx.compose.runtime.*
 import androidx.compose.ui.*
 import androidx.compose.ui.unit.dp
 
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.MoreVert
+
 @Composable
-fun App() {
+fun CompleteApp() {
+    Scaffold(
+        modifier = Modifier.safeDrawingPadding(),
+        topBar = { AppTopBar() },
+    ) { paddingValues ->
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(paddingValues)
+        ) {
+            AppBody()
+        }
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun AppTopBar() {
+    var expanded by remember { mutableStateOf(false) }
+    
+    TopAppBar(
+        title = { NameCard() },
+        actions = {
+            IconButton(onClick = { expanded = true }) {
+                Icon(Icons.Default.MoreVert, contentDescription = "Options")
+            }
+            DropdownMenu(
+                expanded = expanded,
+                onDismissRequest = { expanded = false }
+            ) {
+                DropdownMenuItem(
+                    text = { Text("Settings") },
+                    onClick = { /* Handle click */ expanded = false }
+                )
+
+                HorizontalDivider()
+
+                DropdownMenuItem(
+                    text = { Text("About") },
+                    onClick = { /* Handle click */ expanded = false }
+                )
+            }
+        },
+    ) 
+}
+
+@Composable
+fun AppBody() {
     Column(
         modifier = Modifier
             .fillMaxSize()
             .padding(5.dp)
     ) {
-        Text("Hello, world")
+        Text("Hello, world!")
     }
 }
--- src/main.kt
+++ src/main.kt
@@ -8,19 +8,13 @@ import androidx.compose.foundation.layout.*
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.*
-import org.kotfind.android_course.App
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            Surface(modifier = Modifier.fillMaxSize()) {
-                Column(modifier = Modifier.safeDrawingPadding()) {
-                    NameCard()
-                    App()
-                }
-            }
+            CompleteApp()
         }
     }
 }
