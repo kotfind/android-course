c169ed6570e594a6b6f917df177a6316b3082b47
kotfind <kotfind@yandex.ru>
2025-03-27 22:33:30 +0300

    Colour selection

--- src/app.kt
+++ src/app.kt
@@ -9,6 +9,13 @@ import androidx.compose.ui.unit.dp
 
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.MoreVert
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.style.TextAlign
+
+import androidx.compose.foundation.gestures.detectTapGestures
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.input.pointer.pointerInput
+import androidx.compose.ui.unit.DpOffset
 
 @Composable
 fun CompleteApp() {
@@ -59,11 +66,188 @@ fun AppTopBar() {
 
 @Composable
 fun AppBody() {
-    Column(
+    var background by remember {
+        mutableStateOf(NamedColor.initialBackground)
+    }
+
+    var foreground by remember {
+        mutableStateOf(NamedColor.initialForeground)
+    }
+
+    var menusState by remember {
+        mutableStateOf<MenusState>(MenusState.None)
+    }
+
+    var outerOffset by remember {
+        mutableStateOf(DpOffset(0.dp, 0.dp))
+    }
+
+    var innerOffset by remember {
+        mutableStateOf(DpOffset(0.dp, 0.dp))
+    }
+
+    Box(
         modifier = Modifier
             .fillMaxSize()
-            .padding(5.dp)
+            .padding(5.dp),
     ) {
-        Text("Hello, world!")
+        Box(
+            modifier = Modifier
+                .fillMaxWidth()
+                .background(background.color)
+                .align(Alignment.TopStart)
+                .pointerInput(Unit) {
+                    detectTapGestures(
+                        onLongPress = {
+                            menusState = MenusState.OuterOnly
+                            outerOffset = DpOffset(it.x.dp, it.y.dp)
+                        }
+                    )
+                },
+        ) {
+            Text(
+                text = "Long press me!",
+                color = foreground.color,
+                modifier = Modifier
+                    .align(Alignment.Center)
+                    .padding(20.dp)
+            )
+        }
+
+        DropdownMenu(
+            expanded = menusState.isOuterExpanded,
+            onDismissRequest = { menusState = MenusState.None },
+            offset = outerOffset
+        ) {
+            DropdownMenuItem(
+                onClick = { /* Handled in text */ },
+                text = {
+                    Box(
+                        modifier = Modifier
+                            .fillMaxSize()
+                            .pointerInput(Unit) {
+                                detectTapGestures {
+                                    innerOffset = DpOffset(it.x.dp, it.y.dp) + outerOffset
+                                    menusState = MenusState.Background
+                                }
+                            }
+                    ) {
+                        Text("Background")
+                    }
+                },
+            )
+
+            DropdownMenuItem(
+                onClick = { /* Handled in text */ },
+                text = {
+                    Box(
+                        modifier = Modifier
+                            .fillMaxSize()
+                            .pointerInput(Unit) {
+                                detectTapGestures {
+                                    innerOffset = DpOffset(it.x.dp, it.y.dp) + outerOffset
+                                    menusState = MenusState.Foreground
+                                }
+                            }
+                    ) {
+                        Text("Foreground")
+                    }
+                },
+            )
+        }
+
+        ColorMenu(
+            expanded = menusState is MenusState.Background,
+            onColorSelected = {
+                background = it
+                menusState = MenusState.None
+            },
+            onDismiss = { menusState = MenusState.None },
+            offset = innerOffset
+        )
+
+        ColorMenu(
+            expanded = menusState is MenusState.Foreground,
+            onColorSelected = {
+                foreground = it
+                menusState = MenusState.None
+            },
+            onDismiss = { menusState = MenusState.None },
+            offset = innerOffset
+        )
     }
+}
+
+@Composable
+fun ColorMenu(
+    expanded: Boolean,
+    onColorSelected: (NamedColor) -> Unit,
+    onDismiss: () -> Unit,
+    offset: DpOffset
+) {
+    DropdownMenu(
+        expanded = expanded,
+        onDismissRequest = { onDismiss() },
+        offset = offset
+    ) {
+        for (color_ in NamedColor.all) {
+            DropdownMenuItem(
+                text = { Text(color_.name) },
+                onClick = { onColorSelected(color_) },
+            )
+        }
+    }
+}
+
+data class NamedColor(
+    val name: String,
+    val color: Color,
+) {
+    companion object {
+        val initialBackground = NamedColor("LightGray", Color.LightGray)
+        val initialForeground = NamedColor("Black", Color.Black)
+
+        val all = listOf(
+            initialBackground,
+            initialForeground,
+            NamedColor("Blue", Color.Blue),
+            NamedColor("Cyan", Color.Cyan),
+            NamedColor("DarkGray", Color.DarkGray),
+            NamedColor("Gray", Color.Gray),
+            NamedColor("Green", Color.Green),
+            NamedColor("Magenta", Color.Magenta),
+            NamedColor("Red", Color.Red),
+            NamedColor("White", Color.White),
+        )
+    }
+}
+
+sealed class MenusState {
+    object None : MenusState() {
+        override val isOuterExpanded = false
+        override val isForegroundExpanded = false
+        override val isBackgroundExpanded = false
+    }
+
+    object OuterOnly : MenusState() {
+        override val isOuterExpanded = true
+        override val isForegroundExpanded = false
+        override val isBackgroundExpanded = false
+    }
+
+    object Foreground : MenusState() {
+        override val isOuterExpanded = true
+        override val isForegroundExpanded = true
+        override val isBackgroundExpanded = false
+    }
+
+    object Background : MenusState() {
+        override val isOuterExpanded = true
+        override val isForegroundExpanded = false
+        override val isBackgroundExpanded = true
+    }
+
+    abstract val isOuterExpanded: Boolean
+    abstract val isForegroundExpanded: Boolean
+    abstract val isBackgroundExpanded: Boolean
 }
