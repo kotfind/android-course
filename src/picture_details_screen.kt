5e4d4b7aded072efc827737a8784663ec67d979f
kotfind <kotfind@yandex.ru>
2025-03-28 17:33:09 +0300

    Reset transform on double tap

--- src/picture_details_screen.kt
+++ src/picture_details_screen.kt
@@ -13,6 +13,8 @@ import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.asImageBitmap
 import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.foundation.gestures.detectTapGestures
+import androidx.compose.ui.input.pointer.pointerInput
 
 @Composable
 fun PictureDetailsScreen(
@@ -22,8 +24,9 @@ fun PictureDetailsScreen(
     val context = LocalContext.current
 
     var zoom by remember { mutableStateOf(1f) }
-    var rotation by remember { mutableStateOf(0f) }
     var offset by remember { mutableStateOf(Offset.Zero) }
+    var rotation by remember { mutableStateOf(0f) }
+
     var transformableState = rememberTransformableState {
         zoomChange,
         offsetChange,
@@ -46,6 +49,15 @@ fun PictureDetailsScreen(
                 translationX = offset.x,
                 translationY = offset.y,
             )
-            .transformable(transformableState),
+            .transformable(transformableState)
+            .pointerInput(Unit) {
+                detectTapGestures(
+                    onDoubleTap = {
+                        zoom = 1f
+                        offset = Offset.Zero
+                        rotation = 0f
+                    },
+                )
+            }
     )
 }
