3a4577ae1523a2b2536d008d421ce8c1e973c232
kotfind <kotfind@yandex.ru>
2025-03-28 19:48:10 +0300

    Add help message; Done

--- src/top_bar.kt
+++ src/top_bar.kt
@@ -12,7 +12,7 @@ import androidx.compose.ui.res.painterResource
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun TopBar() {
-    var showHelp by remember {
+    var showControlls by remember {
         mutableStateOf(false)
     }
     
@@ -20,7 +20,7 @@ fun TopBar() {
         title = { NameCard() },
         actions = {
             IconButton(
-                onClick = { showHelp = true }
+                onClick = { showControlls = true }
             ) {
                 Icon(
                     painter = painterResource(R.drawable.help),
@@ -31,15 +31,15 @@ fun TopBar() {
         },
     ) 
 
-    if (showHelp) {
+    if (showControlls) {
         AlertDialog(
             modifier = Modifier.padding(10.dp),
-            title = { Text("Help") },
-            text = { HelpText() },
-            onDismissRequest = { showHelp = false },
+            title = { Text("Controlls") },
+            text = { ControllsText(controlls) },
+            onDismissRequest = { showControlls = false },
             confirmButton = {
                 Button(
-                    onClick = { showHelp = false },
+                    onClick = { showControlls = false },
                 ) {
                     Text("Ok")
                 }
@@ -48,12 +48,102 @@ fun TopBar() {
     }
 }
 
+private val controlls = Controlls(listOf(
+    ControllsSection("Album List", listOf(
+        ControllsShortcut(
+            "Back Key",
+            "Go to previous screen."
+        ),
+    )),
+
+    ControllsSection("Album Details", listOf(
+        ControllsShortcut(
+            "Back Key",
+            "Go to previous screen."
+        ),
+    )),
+
+    ControllsSection("Picture Details (Image)", listOf(
+        ControllsShortcut(
+            "Back Key",
+            "Go to previous screen."
+        ),
+        ControllsShortcut(
+            "Pinch with two fingers",
+            "Zoom the picture in/ out."
+        ),
+        ControllsShortcut(
+            "Rotate with two fingers",
+            "Rotate the picture."
+        ),
+        ControllsShortcut(
+            "Drag with one finger",
+            "Move the picture."
+        ),
+        ControllsShortcut(
+            "Double Click",
+            "Reset all transformations."
+        ),
+        ControllsShortcut(
+            "Long Press",
+            "Show info about the picture."
+        ),
+    )),
+
+    ControllsSection("Picture Details (Bottom Bar)", listOf(
+        ControllsShortcut(
+            "Swipe Left",
+            "Go to previous picture."
+        ),
+        ControllsShortcut(
+            "Swipe Right",
+            "Go to next picture."
+        ),
+    ))
+))
+
 @Composable
-private fun HelpText() {
+private fun ControllsText(controlls: Controlls) {
     Column(
         modifier = Modifier
             .verticalScroll(rememberScrollState()),
     ) {
-        Text("TODO")
+        for (section in controlls.sections) {
+            Text(
+                text = "${section.title}:",
+                color = MaterialTheme.colorScheme.primary,
+                style = MaterialTheme.typography.titleMedium,
+            )
+
+            Column(
+                modifier = Modifier.padding(start = 10.dp),
+            ) {
+                for (shortcut in section.shortcuts) {
+                    Text(
+                        text = shortcut.shortcut,
+                        color = MaterialTheme.colorScheme.primary,
+                    )
+
+                    Text(
+                        text = shortcut.description,
+                        modifier = Modifier.padding(10.dp),
+                    )
+                }
+            }
+        }
     }
 }
+
+private data class Controlls(
+    val sections: List<ControllsSection>,
+)
+
+private data class ControllsSection(
+    val title: String,
+    val shortcuts: List<ControllsShortcut>,
+)
+
+private data class ControllsShortcut(
+    val shortcut: String,
+    val description: String,
+)
