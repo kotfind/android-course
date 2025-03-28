b06a6a46451fb1f5a0f443875e57e2a45e322b67
kotfind <kotfind@yandex.ru>
2025-03-28 17:15:02 +0300

    Transform gestures

--- src/picture_details_screen.kt
+++ src/picture_details_screen.kt
@@ -7,8 +7,12 @@ import androidx.compose.runtime.*
 import androidx.compose.ui.*
 import androidx.compose.ui.unit.dp
 
-import androidx.compose.ui.platform.LocalContext
+import androidx.compose.foundation.gestures.rememberTransformableState
+import androidx.compose.foundation.gestures.transformable
+import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.asImageBitmap
+import androidx.compose.ui.graphics.graphicsLayer
+import androidx.compose.ui.platform.LocalContext
 
 @Composable
 fun PictureDetailsScreen(
@@ -17,9 +21,31 @@ fun PictureDetailsScreen(
 ) {
     val context = LocalContext.current
 
+    var zoom by remember { mutableStateOf(1f) }
+    var rotation by remember { mutableStateOf(0f) }
+    var offset by remember { mutableStateOf(Offset.Zero) }
+    var transformableState = rememberTransformableState {
+        zoomChange,
+        offsetChange,
+        rotationChange,
+        ->
+        zoom *= zoomChange
+        rotation += rotationChange
+        offset += offsetChange * zoom
+    }
+
     Image(
-        modifier = modifier.fillMaxSize(),
         bitmap = picture.getImage(context).asImageBitmap(),
         contentDescription = null,
+        modifier = modifier
+            .fillMaxSize()
+            .graphicsLayer(
+                scaleX = zoom,
+                scaleY = zoom,
+                rotationZ = rotation,
+                translationX = offset.x,
+                translationY = offset.y,
+            )
+            .transformable(transformableState),
     )
 }
