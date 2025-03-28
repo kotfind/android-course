8bb6801ea1682cb5989eb5facce5f577e2ed1935
kotfind <kotfind@yandex.ru>
2025-03-28 16:40:50 +0300

    Handle back button press

--- src/app.kt
+++ src/app.kt
@@ -8,7 +8,9 @@ import androidx.compose.ui.*
 import androidx.compose.ui.unit.dp
 
 import android.Manifest
+import android.app.Activity
 import androidx.activity.ComponentActivity
+import androidx.activity.compose.BackHandler
 import androidx.compose.ui.platform.LocalContext
 
 import android.util.Log
@@ -45,6 +47,8 @@ private fun Master(albums: List<Album>) {
         mutableStateOf<Screen>(Screen.AlbumList())
     }
 
+    BackPressHandler(screen, { screen = it })
+
     val screen_ = screen
     when (screen_) {
         is Screen.AlbumList -> {
@@ -64,7 +68,10 @@ private fun Master(albums: List<Album>) {
                 album = album,
                 modifier = Modifier.fillMaxSize(),
                 onPictureSelected = {
-                    screen = Screen.PictureDetails(it)
+                    screen = Screen.PictureDetails(
+                        album = album,
+                        picture = it,
+                    )
                 },
             )
         }
@@ -81,10 +88,38 @@ private fun Master(albums: List<Album>) {
 
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+private fun BackPressHandler(
+    screen: Screen,
+    onScreenChanged: (Screen) -> Unit,
+) {
+    val activity = LocalContext.current as Activity
+
+    BackHandler(enabled = true) {
+        when (screen) {
+            is Screen.AlbumList -> {
+                activity.finish()
+            }
+
+            is Screen.AlbumDetails -> {
+                onScreenChanged(Screen.AlbumList())
+            }
+
+            is Screen.PictureDetails -> {
+                onScreenChanged(Screen.AlbumDetails(screen.album))
+            }
+        }
+    }
+}
+
 private sealed class Screen {
     class AlbumList : Screen()
 
     class AlbumDetails(val album: Album) : Screen()
 
-    class PictureDetails(val picture: Picture) : Screen()
+    class PictureDetails(
+        val album: Album,
+        val picture: Picture
+    ) : Screen()
 }
